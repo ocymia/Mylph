c16f2a59b28c4d5bc2a9e0be674f957f1fbf6328
ocymia <jerry_kapp@hotmail.com>
2016-02-08 16:10:24 +0100

    admin stuff and temp index for debugging

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TEMP</title>
+</head>
+<body>
+Juste en attendant d avoir un acceuil afin de donner easy access aux trux
+
+<ul>
+	<li>
+		<a href="inc/add_location.php">add_location.php</a>
+	</li>
+	<li>
+		<a href="inc/signup.php">signup.php</a>
+	</li>
+	<li>
+		<a href="inc/login.php">login.php</a>
+	</li>
+
+
+</ul>
+
+
+</body>
+</html>
