ddeaa4d420c99ff324dd0c558bd07f54ad72ad12
ginma89 <max@ginter-wersant.lu>
2016-02-08 16:30:12 +0100

    added loop to display locations in accueil.php

--- inc/accueil.php
+++ inc/accueil.php
@@ -1,3 +1,4 @@
+<?php require 'header.php'; ?>
 <pre>
 <?php
  
@@ -9,7 +10,20 @@ $getLocations="
 ";
 $pdoStatement=$pdo->prepare($getLocations);
 $pdoStatement->execute();
-$result=$pdoStatement->fetchAll();
-print_r($result);
+$results=$pdoStatement->fetchAll();
+//print_r($results);
+foreach ($results as $key => $value) {
+	echo 'NEW LOC:<br/>';
+	echo 'Name of location: '.$value['loc_name'].'<br/>';
+	echo 'Address of location: '.$value['loc_adr'].'<br/>';
+	echo 'Postal code: '.$value['loc_cp'].'<br/>';
+	echo 'City: '.$value['loc_city'].'<br/>';
+	echo 'Description: '.$value['loc_desc'].'<br/>';
+	echo 'IMAGE: '.$value['loc_img'].'<br/>';
+	echo 'x?: '.$value['loc_x'].'<br/>';
+	echo 'y?: '.$value['loc_y'].'<br/>';
+	echo '<br/>';
+}
 ?>
 </pre>
+<?php require 'footer.php'; ?>
