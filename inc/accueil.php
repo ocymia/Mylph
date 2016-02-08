c98738111f436189a9c777b4e436379e1496207f
ginma89 <max@ginter-wersant.lu>
2016-02-08 16:49:29 +0100

    dunno anymore... im tired

--- inc/accueil.php
+++ inc/accueil.php
@@ -12,18 +12,17 @@ $pdoStatement=$pdo->prepare($getLocations);
 $pdoStatement->execute();
 $results=$pdoStatement->fetchAll();
 //print_r($results);
-foreach ($results as $key => $value) {
-	echo 'NEW LOC:<br/>';
-	echo 'Name of location: '.$value['loc_name'].'<br/>';
-	echo 'Address of location: '.$value['loc_adr'].'<br/>';
-	echo 'Postal code: '.$value['loc_cp'].'<br/>';
-	echo 'City: '.$value['loc_city'].'<br/>';
-	echo 'Description: '.$value['loc_desc'].'<br/>';
-	echo 'IMAGE: '.$value['loc_img'].'<br/>';
-	echo 'x?: '.$value['loc_x'].'<br/>';
-	echo 'y?: '.$value['loc_y'].'<br/>';
-	echo '<br/>';
-}
+foreach ($results as $key => $value) { ?>
+	<div>
+		<?php
+			echo $value['loc_name'].'<br/>';
+			echo $value['loc_adr'].'<br/>';
+			echo $value['loc_cp'].'<br/>';
+			echo $value['loc_city'].'<br/>';
+			echo $value['loc_desc'].'<br/>';
+		?>
+	</div>
+<?php }
 ?>
 </pre>
 <?php require 'footer.php'; ?>
