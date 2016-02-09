ec9b4b6b299042fd21db9acc3e2a956f087a9929
ocymia <jerry_kapp@hotmail.com>
2016-02-09 09:20:04 +0100

    again sql ini

---
+++ inisql/initialize_loc_types.php
@@ -0,0 +1,23 @@
+<?php
+//initialize db location types
+//conenction details for db // contains $pdo
+require '../inc/config.php';
+
+
+$rowcount_types = "SELECT * FROM loctype";
+$pdoCount = $pdo->query($rowcount_types);
+
+
+if ($pdoCount->rowCount() == 0){
+	//create types
+	$sqltypes="
+		INSERT INTO loctype (typ_id,typ_name)
+		VALUES (1,'Restaurant'),(2,'Entertainment'),(3,'Accessibility'),(4,'Other')
+	";
+	$pdoStatement = $pdo->query($sqltypes);
+} else {
+	//do nothin - types seem to exist already
+}
+
+
+?>
